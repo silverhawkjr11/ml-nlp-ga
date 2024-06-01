3ceb9bb4058f1d7e58dc19e557a7bd9f915ed665
saeed.kanawat <saeed.kanawat@gmail.com>
2024-06-01 06:14:12 +0000

    idk what happened here

--- src/ga.py
+++ src/ga.py
@@ -2,62 +2,87 @@ import numpy
 import random
 from deap import base, creator, tools, algorithms
 from joblib import load
+import nltk
+from nltk import pos_tag
+from nltk.corpus import words
 
-# Create the fitness class, which will be the base of the individual
-creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-text_clf = load('best_model.joblib')
+# Ensure NLTK resources are downloaded
+nltk.download('averaged_perceptron_tagger')
+nltk.download('words')
 
-# Create the individual class based on list with fitness attribute
-creator.create("Individual", list, fitness=creator.FitnessMax)
-def get_words(filename):
-    with open(filename, 'r') as f:
-        words = f.read().splitlines()
-    return words
+class GeneticAlgorithm:
+    def __init__(self, model_path, dictionary_path, reduced_dict_size=10000):
+        self.text_clf = load(model_path)
+        self.final_dictionary = self.get_words(dictionary_path)
+        self.reduced_dict = self.get_random_words(dictionary_path, reduced_dict_size)
+        self.word_list = words.words()
+        self.pos_dict = self.create_pos_dict()
 
-final_dictionary = get_words("new_dictionary.txt")
-import random
+        # Create the fitness class, which will be the base of the individual
+        creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+        # Create the individual class based on list with fitness attribute
+        creator.create("Individual", list, fitness=creator.FitnessMax)
+    
+    def get_words(self, filename):
+        with open(filename, 'r') as f:
+            words = f.read().splitlines()
+        return words
 
-def get_random_words(filename, n):
-    with open(filename, 'r') as f:
-        words = f.read().splitlines()
-    return random.sample(words, n)
+    def get_random_words(self, filename, n):
+        with open(filename, 'r') as f:
+            words = f.read().splitlines()
+        if n > len(words):
+            n = len(words)  # Adjust n to the size of the dictionary if n is larger
+        return random.sample(words, n)
 
-# Usage
-reduced_dict = get_random_words('dictionary.txt', 10000)
-# Define the fitness function
-def fitness(individual):
-    sentence = " ".join(individual)
-    score = text_clf.predict([sentence])[0]
-    return score,
+    def get_pos(self, word):
+        return pos_tag([word])[0][1]
 
-# Define the mutation function
-def mutate(individual):
-    index = random.randrange(len(individual))
-    individual[index] = random.choice(reduced_dict)
-    return individual,
-def ga():
-    # Create the toolbox
-    toolbox = base.Toolbox()
-    toolbox.register("individual", tools.initRepeat, creator.Individual, lambda: random.choice(final_dictionary), n=20)
-    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-    toolbox.register("evaluate", fitness)
-    toolbox.register("mate", tools.cxTwoPoint)
-    toolbox.register("mutate", mutate)
-    toolbox.register("select", tools.selTournament, tournsize=3)
+    def create_pos_dict(self):
+        pos_dict = {}
+        for word in self.word_list:
+            pos = self.get_pos(word)
+            if pos not in pos_dict:
+                pos_dict[pos] = []
+            pos_dict[pos].append(word)
+        return pos_dict
 
-    # Initialize a population
-    pop = toolbox.population(n=300)
+    def fitness(self, individual):
+        sentence = " ".join(individual)
+        score = self.text_clf.predict([sentence])[0]
+        return score,
 
-    # Define the hall of fame
-    hof = tools.HallOfFame(1)
+    def mutate(self, individual):
+        index = random.randrange(len(individual))
+        original_word = individual[index]
+        original_pos = self.get_pos(original_word)
+        if original_pos in self.pos_dict:
+            individual[index] = random.choice(self.pos_dict[original_pos])
+        return individual,
 
-    # Perform the genetic algorithm
-    stats = tools.Statistics(lambda ind: ind.fitness.values)
-    stats.register("avg", numpy.mean)
-    stats.register("min", numpy.min)
-    stats.register("max", numpy.max)
+    def run(self):
+        # Create the toolbox
+        toolbox = base.Toolbox()
+        toolbox.register("individual", tools.initRepeat, creator.Individual, lambda: random.choice(self.final_dictionary), n=20)
+        toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+        toolbox.register("evaluate", self.fitness)
+        toolbox.register("mate", tools.cxTwoPoint)
+        toolbox.register("mutate", self.mutate)
+        toolbox.register("select", tools.selTournament, tournsize=3)
 
-    pop, log = algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.2, ngen=40, stats=stats, halloffame=hof, verbose=True)
-    sentence = " ".join(hof[0])
-    # Print the best sentence found
-    return (f"the best sentence is: \"{sentence}\", with score: {hof[0].fitness.values}")
+        # Initialize a population
+        pop = toolbox.population(n=300)
+
+        # Define the hall of fame
+        hof = tools.HallOfFame(1)
+
+        # Perform the genetic algorithm
+        stats = tools.Statistics(lambda ind: ind.fitness.values)
+        stats.register("avg", numpy.mean)
+        stats.register("min", numpy.min)
+        stats.register("max", numpy.max)
+
+        pop, log = algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.2, ngen=40, stats=stats, halloffame=hof, verbose=True)
+        sentence = " ".join(hof[0])
+        # Return the best sentence found
+        return f"The best sentence is: \"{sentence}\", with score: {hof[0].fitness.values[0]}"
--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
 import os
 from joblib import load
-from flask import Flask, send_file
-from flask import request, render_template
-from ga import *
+from flask import Flask, send_file, request, render_template
+from ga import GeneticAlgorithm
+
 app = Flask(__name__, template_folder='template')
 
-@app.route("/")
+# Initialize GeneticAlgorithm instance
+ga_instance = GeneticAlgorithm(
+    model_path='best_model.joblib',
+    dictionary_path='new_dictionary.txt'
+)
 
+@app.route("/")
 def index():
     return send_file('index.html')
 
@@ -18,14 +23,14 @@ def predict():
         prediction = classifier.predict([sentence])
         return render_template('predict.html', prediction=prediction)
     return render_template('predict.html')
+
 @app.route("/generate", methods=['GET'])
 def generate():
-    generated_results = ga()
+    generated_results = ga_instance.run()
     return render_template('generate.html', generated_results=generated_results)
 
 def main():
-    app.run(port=int(os.environ.get('PORT', 80
-)))
+    app.run(port=int(os.environ.get('PORT', 3000)))
 
 if __name__ == "__main__":
     main()
